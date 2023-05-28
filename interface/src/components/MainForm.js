7fb3682c3d3c26055a1c01bed743dbd88762bd51
EL MEHDI BOUTOUMIT <117985339+mehdiboutoumit@users.noreply.github.com>
2023-05-28 12:18:13 +0000

    Customize the result card

--- interface/src/components/MainForm.js
+++ interface/src/components/MainForm.js
@@ -1,23 +1,27 @@
 import React, { useState } from 'react';
 
 import { Form, Button, Card } from 'react-bootstrap';
-
-
-import axios from 'axios';
-
+import './form.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
+import { useEffect } from 'react';
+
+import axios from 'axios';
 
 export default function MainForm() {
   const [taux, setTaux] = useState('');
   const [files,setFiles] = useState([]);
+  const [colorR ,setColorR] = useState('white');
+  const [num,setNum] =useState(0);
+  const [message, setMessage]= useState('');
+  const [it,setIt] =useState(1);
   
-   
-  const handleSubmit = async () => {
+    const handleSubmit = async () => {
     const textInput = document.getElementById('text');
     const text = textInput.value;
     const fileInput = document.getElementById('file');
     const file = fileInput.files;
+   
   
     try {
       const formData = new FormData();
@@ -33,73 +37,96 @@ export default function MainForm() {
   
       setTaux("Le pourcentage de plagiat est : " + responseData[0]);
       setFiles(filesData);
+      setNum(responseData[0]);
+      setIt(2)
+   
+ 
+      
     } catch (error) {
       console.error(error);
       // Handle error if necessary
     }
   };
-  const getTauxColor = () => {
-    // Define the color range based on the taux value
-    const red = Math.round((1 - taux) * 255);
-    const green = Math.round(taux * 255);
-    const blue = 0;
-    return `rgb(${red}, ${green}, ${blue})`;
-  };
   
-
+  useEffect(() => {
+    // Run whenever the `taux` value changes
+    const hue = 120 - (num * 120) / 100; // Calculate hue value based on num, with 0 as green and 100 as red
+    const colorValue = `hsl(${hue}, 100%, 50%)`; // Set the color value using the calculated hue
+if(it==2)
+    setColorR(colorValue);
+    if(num == 0) setMessage('Le texte est original.');
+    if(num == 100) setMessage('Le texte est entièrement plagié !');
+    if(num!=0 && num!=100) setMessage('');
+  
+  }, [num]);
+ 
+    // Define the color range based on the taux value
+   
+   
+  
+  
+  
   return (
-    <div className="container text-center">
-    <div className="col-md-9 mx-auto">
-      <Form>
-        <Form.Group controlId="text">
-          <Form.Label>Saisir le texte :</Form.Label>
-          <Form.Control as="textarea" rows={7} columns={9} />
-        </Form.Group>
+    <>
+   <div className="container" >
+      <div >
+        <Form style={{ width: '97%', margin : '1%', textAlign: 'center'}}>
+          <Form.Group controlId="text">
+            <Form.Label style={{  fontSize : '20px' }}>Saisir le texte :</Form.Label>
+            <Form.Control as="textarea" rows={7} />
+          </Form.Group>
 
-        <Form.Group controlId="file">
-          <Form.Label>Choisir un fichier :</Form.Label>
-          <Form.Control type="file" multiple />
-        </Form.Group>
+          <Form.Group controlId="file">
+            <Form.Label style={{  fontSize : '20px' }}>Choisir les fichiers de comparaison :</Form.Label>
+            <Form.Control type="file" multiple  />
+          </Form.Group>
 
-        <Button variant="primary" type="button" onClick={handleSubmit}>
-          Comparer
-        </Button>
-      </Form>
-
-      <Card className="mt-4">
-        <Card.Body>
-          <Card.Title>Résultats</Card.Title>
-          {taux && (
-            <div className="mb-3">
-              <Form.Label htmlFor="taux" className="form-label result-label">
-                Taux de plagiat :
-              </Form.Label>
-              <Card.Text id="taux" className="result-value">
-              <div
-                  id="taux"
-                  className="result-value"
-                  style={{ color: getTauxColor() }}
-                >
-                  {taux}
-                </div>
-              </Card.Text>
-            </div>
-          )}
-          {files.length > 0 && (
-            <div>
-              <Form.Label className="form-label result-label">
-                Fichiers similaires :
-              </Form.Label>
-              {files.map((file, index) => (
-                <Card.Text key={index} className="result-value">
-                  {file}
-                </Card.Text>
-              ))}
-            </div>
-          )}
-        </Card.Body>
-      </Card>
+          <Button variant="primary" style={{ width: '20%'}}  type="button" onClick={handleSubmit}>
+            Comparer
+          </Button>
+        </Form>
+      </div>
     </div>
-    
-  </div>  )};
+    {taux && (
+        
+            <Card style={{ width: '80%', margin : '1%' , backgroundColor : '#fffbff' , textAlign: 'center', border : '2px' , borderStyle : 'solid'}}>
+               <Card.Body>
+              <Card.Title style={{  backgroundColor : colorR, fontSize : '40px' }} >Résultats</Card.Title>
+             
+              {taux && (
+                <div className="mb-3" style={{  fontSize : '30px' ,}} >
+                 
+                  <div
+                    id="taux"
+                    
+                    style={{ color: colorR }}
+                  >
+                    {taux} %
+                  </div>
+                  {message && <div>{message}</div>}
+                </div>
+              )}
+              <hr></hr>
+              {files.length > 0 && (
+                <div style={{  fontSize : '20px' , }}>
+                  <Form.Label style={{  fontSize : '20px' , fontStyle : 'bold'}}>
+                    Le taux de similitude entre le texte et chaque document :
+                  </Form.Label >
+                  <div>
+                  {files.map((file, index) => (
+                    <Card.Text key={index} >
+                      {file}
+                    </Card.Text>
+                  ))}
+                  </div>
+                </div>
+              )}
+            
+            </Card.Body>
+       </Card>
+      
+      )}
+  
+</>
+  )};
   
